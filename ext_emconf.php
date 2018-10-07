87266703325fb6b7851b41aceb801418cf2a3591
Benjamin Riezler <b.riezler@pixel-ink.de>
2018-10-07 13:23:09 +0200

    [TASK] Set requirment to TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '3.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.0.0-9.4.99',
+			'typo3' => '8.0.0-9.5.99',
 		],
 		'conflicts' => [
 		],
